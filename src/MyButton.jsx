f30fe4eef80e8fe805f56e182359616aab6695c1
Mayhem-fok <Reynold.may@georgebrown.ca>
2026-09-22 11:25:26 -0400

    Flatten my-react-app into project root

---
+++ src/MyButton.jsx
@@ -0,0 +1,10 @@
+const MyButton = ({ count, onClick }) => {
+
+    return (
+      <button onClick={onClick}>
+        Clicked {count} times
+      </button>
+    );
+  }
+
+export default MyButton;
---
+++ src/button.jsx
@@ -0,0 +1,16 @@
+// function LearnMoreButton({ onClick }) {
+//   return (
+//     <button className="learnMoreButton" onClick={onClick}>
+// .learnMoreButton {
+//   background-color: #841584;
+//   color: white;
+//   border: none;
+//   padding: 8px 16px;
+//   border-radius: 6px;
+//   cursor: pointer;
+//   font-size: 14px;
+// }
+
+// .learnMoreButton:hover {
+//   background-color: #6b1169;
+// }
